32f95a24899f517fa2be8e34b66e493d3a4f1348
cajicek55 <ahoj327@azet.sk>
2014-11-12 00:03:23 +0100

    commit_06
    poriešená kamera
    vyriešené ničenie postáv

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,49 +4,61 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
 
 	private Vector3 pos;
-	private Vector3[] positions;
-//	private float[] weights;
+//	private Vector3[] positions;
 	float distance = 0;
 	Vector3 max;
 	Vector3 min;
 
 	// Use this for initialization
 	void Start () {
-		positions = new Vector3[GameObject.FindGameObjectsWithTag("Player").Length];
-//		weights = new float[positions.Length];
-//		for (int i=0; i<weights.Length; i++) {
-//			weights[i]=1;
-//		}
+		//pri zrušeni ktorehokolvek hrača pride k OutOfBounds
+		//positions = new Vector3[GameObject.FindGameObjectsWithTag("Player").Length];
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-		//zistenie polohy vsetkych hracov
-		Vector3 curPos = new Vector3(0,0,0);
-		for (int i=0; i<positions.Length; i++) {
-			positions[i] = GameObject.FindGameObjectsWithTag("Player")[i].transform.position;
-			curPos.x += positions[i].x; //* weights[i];
-			curPos.y += positions[i].y; //* weights[i];
-			curPos.z += positions[i].z;
+		if (GameObject.FindGameObjectsWithTag ("Player").Length > 1) {
+						//zistenie polohy vsetkych hracov
+						Vector3 curPos = new Vector3 (0, 0, 0);
+						for (int i=0; i<GameObject.FindGameObjectsWithTag("Player").Length; i++) {
+								//positions[i] = GameObject.FindGameObjectsWithTag("Player")[i].transform.position;
+								curPos.x += GameObject.FindGameObjectsWithTag ("Player") [i].transform.position.x;
+								curPos.y += GameObject.FindGameObjectsWithTag ("Player") [i].transform.position.y;
+								curPos.z += GameObject.FindGameObjectsWithTag ("Player") [i].transform.position.z;
+						}
+						curPos.x = curPos.x / GameObject.FindGameObjectsWithTag ("Player").Length;
+						curPos.y = curPos.y / GameObject.FindGameObjectsWithTag ("Player").Length;
+						curPos.z = (curPos.z-100) / GameObject.FindGameObjectsWithTag ("Player").Length;
+
+						//pocitanie "vah"
+
+
+						max = GameObject.FindGameObjectsWithTag ("Player") [0].transform.position;
+						min = GameObject.FindGameObjectsWithTag ("Player") [0].transform.position;
+						for (int i=0; i<GameObject.FindGameObjectsWithTag("Player").Length; i++) {
+								if (max.x < GameObject.FindGameObjectsWithTag ("Player") [i].transform.position.x)
+										max = GameObject.FindGameObjectsWithTag ("Player") [i].transform.position;
+								if (min.x > GameObject.FindGameObjectsWithTag ("Player") [i].transform.position.x)
+										min = GameObject.FindGameObjectsWithTag ("Player") [i].transform.position;
+						}
+						distance = Mathf.Sqrt ((max.x - min.x) * (max.x - min.x) + (max.y - min.y) * (max.y - min.y));
+		
+						if (distance > 8) {
+								curPos.x += Mathf.Abs ((1 / distance) * max.x);
+								curPos.y += (1 / distance) * max.y;
+						}
+						//pohyb kamery medzi vsetkych hracov
+						Camera.main.transform.position = curPos;
+				} else {
+			//Camera.main.transform.position = GameObject.FindGameObjectWithTag("Player").transform.position;
+			Vector3 curPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+			curPos.z -= 100;
+			Camera.main.transform.position = curPos;
 		}
-		curPos.x = curPos.x / positions.Length;
-		curPos.y = curPos.y / positions.Length;
-
-		//pocitanie vah
-
-		/*Vector3*/ max = positions[0];
-		/*Vector3*/ min = positions[0];
-//		for(int i=0;i<GameObject.FindGameObjectsWithTag("Player").Length;i++){
-//			if(max.x < positions[i].x) max = positions[i];
-//			if(min.x > positions[i].x) min = positions[i];
-//		}
-//		distance = Mathf.Sqrt((max.x-min.x)*(max.x-min.x)+(max.y-min.y)*(max.y-min.y));
-//		curPos.x += Mathf.Abs((1/distance)*max.x);
-//		curPos.y += (1/distance)*max.y;
-
-		//pohyb kamery medzi vsetkych hracov
-		Camera.main.transform.position = curPos;
-
+		//vyhra jedneho z hracov
+		if (GameObject.FindGameObjectsWithTag ("Player").Length <= 1) {
+				
+		}
 
 		//zmensovanie Field of View
 		if(Camera.main.orthographicSize > 2.5f) Camera.main.orthographicSize = Camera.main.orthographicSize - 0.0001f;
