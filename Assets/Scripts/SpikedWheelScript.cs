e6986bf87b2895ee3ba44aaca232552e42c8c928
evulilienka <drabova.eva@gmail.com>
2014-11-18 04:09:44 +0100

    commit_10

--- Assets/Scripts/SpikedWheelScript.cs
+++ Assets/Scripts/SpikedWheelScript.cs
@@ -2,19 +2,14 @@
 using System.Collections;
 
 public class SpikedWheelScript : MonoBehaviour {
-<<<<<<< HEAD
 	void Update () {
 		transform.Rotate(Vector3.back *70 * Time.deltaTime);
 		
 	}
 	void OnTriggerEnter2D(Collider2D collider){
-		Destroy(collider.gameObject);
-
-
-=======
-
-	void OnTriggerEnter2D(Collider2D collider){
-		Destroy(collider.gameObject);
->>>>>>> origin/master
-	}
+		if(collider.gameObject.tag == "Player"){
+			Destroy(collider.gameObject);
+		}
+		
+}
 }
