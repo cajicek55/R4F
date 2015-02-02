fe4e0d65f13ace24a2af4547c7c5625b5e202a8c
cajicek55 <ahoj327@azet.sk>
2015-02-02 17:53:18 +0100

    commit_29
    dokončený level

---
+++ Assets/Scripts/ClockworkLevel/WarningScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarningScript : MonoBehaviour {
+	public GUIText text;
+	//private int timer = 200;
+
+	void OnTriggerEnter2D(Collider2D hit){
+		if(hit.gameObject.tag == "Player"){
+			text.text = "Floating platform ahead";
+		}
+
+
+	}
+
+	void OnTriggerExit2D(Collider2D hit){
+		if(hit.gameObject.tag == "Player"){
+			text.text = "";
+		}
+
+	}
+}
